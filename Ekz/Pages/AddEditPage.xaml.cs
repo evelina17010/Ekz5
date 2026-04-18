64c24534302dda9f341b504029f467aa406f2626
Student22_4 <Student22_4@22KAB-4>
2026-04-18 14:55:38 +0300

    Добавьте файлы проекта.

---
+++ Ekz/Pages/AddEditPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Ekz.Conn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ekz.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        Product product;
+        public AddEditPage(Product _product)
+        {
+            InitializeComponent();
+           product = _product;
+            this.DataContext = product;
+            cmbType.ItemsSource= DB.decorEntities.TypeProd.ToList();
+            cmbType.DisplayMemberPath = "Name";
+            App.mainWindow.txbMain.Text = "ДОБАВЛЕНИЕ/РЕДАКТИРОВАНИЕ";
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               
+                if (product.Id == 0)
+                {
+                    product.IdType = (cmbType.SelectedItem as TypeProd).ID;
+                    DB.decorEntities.Product.Add(product);
+                }
+                DB.decorEntities.SaveChanges();
+                MessageBox.Show("СОХРАНЕНО", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                NavigationService.Navigate(new ToDoList());
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void btnOtm_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ToDoList());
+        }
+    }
+}
---
+++ Ekz/Pages/ToDoList.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Ekz.Conn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ekz.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ToDoList.xaml
+    /// </summary>
+    public partial class ToDoList : Page
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+            App.mainWindow.txbMain.Text = "СПИСОК";
+            lstData.ItemsSource= DB.decorEntities.Product.ToList(); 
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEditPage(new Product()));
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var select = lstData.SelectedItem as Product;
+                if ( select!= null)
+                {
+                    NavigationService.Navigate(new AddEditPage(select));
+                }
+                else MessageBox.Show("Выберите запись для изменения","Уведомление",MessageBoxButton.OK,MessageBoxImage.Information);
+            }
+            catch(Exception ex) {MessageBox.Show(ex.Message); }
+        }
+
+        private void lstData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var select= lstData.SelectedItem as Product;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstData.ItemsSource = DB.decorEntities.Product.ToList();
+        }
+    }
+}
